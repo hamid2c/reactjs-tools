e6c0a5106baa6de4a6b9d0108ae8b7ef40370eb9
Hamid <hamid2c@gmail.com>
2018-07-08 22:19:41 +0200

    work on pack script

--- pipeline/pack.js
+++ pipeline/pack.js
@@ -20,14 +20,18 @@ if (process.argv.length == 4) {
 }
 
 const packaje_json_path = path.join(proj_dir, "package.json");
-
 const read_text = fs.readFileSync(packaje_json_path);
 var obj = JSON.parse(read_text, "utf-8");
-console.log(out_dir);
-console.log(obj);
-console.log("===========");
-
+// Modify the homepage property of package.json
 obj.homepage = out_dir;
-
 fs.writeFileSync(packaje_json_path, JSON.stringify(obj))
+
+// Copy our webpack configuration file to the react project directory
+const webpack_config_filename = "node_modules/react-scripts/config/webpack.config.prod.js";
+const our_webpack_config = "webpack.config.js"
+
+const program_dir = path.dirname(require.main.filename);
+const our_webpack_config_path = path.join(program_dir, our_webpack_config);
+fs.copyFileSync(our_webpack_config_path, 
+    path.join(proj_dir, webpack_config_filename));
 

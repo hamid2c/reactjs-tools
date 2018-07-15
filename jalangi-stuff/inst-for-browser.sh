29fd0d43bec48958d1116bcd6c5247ec6af68cc0
Hamid <hamid2c@gmail.com>
2018-07-15 14:42:10 +0200

    experimenting

--- jalangi-stuff/inst-for-browser.sh
+++ jalangi-stuff/inst-for-browser.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+export JALANGI_HOME=/all/projs/jalangi2
 if [ -z "$JALANGI_HOME" ]; then
     echo "JALANGI_HOME should be set."
     exit 1
 fi
 cd $JALANGI_HOME
+if [ $((0)) == 1 ]; then
+echo "No 1"
 node src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi \
  --analysis src/js/sample_analyses/ChainedAnalyses.js \
  --analysis src/js/sample_analyses/dlint/Utils.js \
@@ -13,7 +16,18 @@ node src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi
  --analysis src/js/sample_analyses/dlint/ShadowProtoProperty.js \
  --analysis src/js/sample_analyses/dlint/ConcatUndefinedToString.js \
  --analysis src/js/sample_analyses/dlint/UndefinedOffset.js \
- --outputDir /tmp /home/hamid/temp/foo/build #tests/tizen/annex
+ --analysis /all/projs/reactjs-tools/jalangi-stuff/cg-analysis.js \
+ --outputDir /tmp/jala /tmp/bla #tests/tizen/annex
+fi
+
+if [ $((1)) == 1 ]; then
+echo "No 2"
+node src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi \
+--analysis src/js/sample_analyses/ChainedAnalyses.js \
+--analysis src/js/runtime/SMemory.js \
+--analysis src/js/sample_analyses/pldi16/TraceAll.js \
+--outputDir /tmp/ja /tmp/new/static/js/main.cd32e3f8.js
+fi
 
 #chromium-browser --incognito /tmp/annex/index.html &
 # open file:///tmp/annex/index.html

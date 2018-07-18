9f262d4e6bbee58700e72abc2533d6db31e5411f
Hamid <hamid2c@gmail.com>
2018-07-18 23:17:59 +0200

    analyses module

---
+++ pipeline.analyses.js
@@ -0,0 +1,14 @@
+module.exports.analyses = [
+     "$JALANGI_HOME/src/js/sample_analyses/ChainedAnalyses.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/Utils.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/CheckNaN.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/FunCalledWithMoreArguments.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/CompareFunctionWithPrimitives.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/ShadowProtoProperty.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/ConcatUndefinedToString.js",
+     "$JALANGI_HOME/src/js/sample_analyses/dlint/UndefinedOffset.js",
+     "cg-analysis.js"
+   ];
+
+   module.exports.baseDirectory = __dirname;
+
